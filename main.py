d8a36af7e03f6806285ce3f9fda7f3481a63171d
Lee Stetson <lee.d.stetson@gmail.com>
2024-02-08 13:54:48 -0500

    Main uses subset for now

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from models import llm, retrieval
 import gradio as gr
 
 
-def run_query(question, db_path="data/videos.db", 
+def run_query(question, db_path="data/videos_subset_more_context.db", #dev path
               num_rel_segments=5, 
-              llm_model="gpt-3.5-turbo-1106", 
-              llm_temp=0.5):
+              llm_model="gpt-3.5-turbo-0125", 
+              llm_temp=0.1):
     
     relevant_segments = retrieval.get_relevant_segments(question,
                                                          db_path=db_path,
